44c190b573598e0651e330e40db9bac02e5e6b44
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-08-11 07:46:16 -0400

    Fixed lesson button styling

--- frontend/src/flow/5-lessons/JavaLessons/JavaVariables/JavaVariables.tsx
+++ frontend/src/flow/5-lessons/JavaLessons/JavaVariables/JavaVariables.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react"
-import Container from "../../../../components/Container"
-import Display from "../../../../components/Display"
 import JavaVariableQuestion1 from "../../../6-questions/JavaQuestions/JavaVariablesQuestions.tsx/JavaVariableQuestion1"
 import JavaVariableQuestion2 from "../../../6-questions/JavaQuestions/JavaVariablesQuestions.tsx/JavaVariableQuestion2"
 import JavaVariableQuestion3 from "../../../6-questions/JavaQuestions/JavaVariablesQuestions.tsx/JavaVariableQuestion3"
@@ -29,34 +27,32 @@ function JavaLesson1({props}:{props:any}) {
 
 
   return (
-    <Container>
-        <Display>
-            <div className="parent-jawn">
-                <div className="child-jawn">
-                    {questionsAnswered === 0 &&
-                        <JavaVariableQuestion1 props={{completeQuestion:completeQuestion}} />
-                    }
-                    {questionsAnswered === 1 &&
-                        <JavaVariableQuestion2 props={{completeQuestion:completeQuestion}} />
-                    }
-                    {questionsAnswered === 2 &&
-                        <JavaVariableQuestion3 props={{completeQuestion:completeQuestion}} />
-                    }
-                    {questionsAnswered === 3 &&
-                        <JavaVariableQuestion4 props={{completeQuestion:completeQuestion}} />
-                    }
-                    {questionsAnswered === 4 &&
-                        <JavaVariableQuestion5 props={{completeQuestion:completeQuestion}} />
-                    }
-                    {questionsAnswered === 5 &&
-                        <div className="question-explanation">
-                            Score: {correctAnswers}/5
-                        </div>
-                    }
-                </div>
+    <>
+        <div className="parent-jawn">
+            <div className="child-jawn">
+                {questionsAnswered === 0 &&
+                    <JavaVariableQuestion1 props={{completeQuestion:completeQuestion}} />
+                }
+                {questionsAnswered === 1 &&
+                    <JavaVariableQuestion2 props={{completeQuestion:completeQuestion}} />
+                }
+                {questionsAnswered === 2 &&
+                    <JavaVariableQuestion3 props={{completeQuestion:completeQuestion}} />
+                }
+                {questionsAnswered === 3 &&
+                    <JavaVariableQuestion4 props={{completeQuestion:completeQuestion}} />
+                }
+                {questionsAnswered === 4 &&
+                    <JavaVariableQuestion5 props={{completeQuestion:completeQuestion}} />
+                }
+                {questionsAnswered === 5 &&
+                    <div className="question-explanation">
+                        Score: {correctAnswers}/5
+                    </div>
+                }
             </div>
-        </Display>
-    </Container>
+        </div>
+    </>
   )
 }
 
