9d5e0c2ab58d1929ca150192c570a446fc2e71fb
LolaaDiamondd <kayladejesus1.hvc@gmail.com>
2024-08-11 08:14:04 -0400

    disabled lessons without content

--- frontend/src/flow/4-select-lesson/SelectJavaLesson/SelectConditionalsLesson.tsx
+++ frontend/src/flow/4-select-lesson/SelectJavaLesson/SelectConditionalsLesson.tsx
@@ -7,51 +7,63 @@ function SelectIfElseLesson({props}:{props:any}) {
     return (
         <div className="lesson-btn-container">
             <button className="lesson-btn"
-                onClick={() => handleButtonClick("Java Conditionals If")}>
+                onClick={() => null}>
+                {/* onClick={() => handleButtonClick("Java Conditionals If")} */}
                     If
             </button>
             <button className="lesson-btn"
-                onClick={() => handleButtonClick("Java Conditionals Else")}>
+                onClick={() => null}>
+                {/* onClick={() => handleButtonClick("Java Conditionals Else")} */}
                     Else
             </button>
             <button className="lesson-btn"
-                onClick={() => handleButtonClick("Java Conditionals Elseif")}>
+                onClick={() => null}>
+                {/* onClick={() => handleButtonClick("Java Conditionals Elseif")} */}
                     Elseif
             </button>
             <button className="lesson-btn"
-                onClick={() => handleButtonClick("Java Conditionals Multiple Ifs")}>
+                onClick={() => null}>
+                {/* onClick={() => handleButtonClick("Java Conditionals Multiple Ifs")} */}
                     Multiple Ifs
             </button>
             <button className="lesson-btn"
-                onClick={() => handleButtonClick("Java Conditionals Multiple Elseifs")}>
+                onClick={() => null}>
+                {/* onClick={() => handleButtonClick("Java Conditionals Multiple Elseifs")} */}
                     Multiple Elseifs
             </button>
             <button className="lesson-btn"
-                onClick={() => handleButtonClick("Java Conditionals If/Else Nesting")}>
+                onClick={() => null}>
+                {/* onClick={() => handleButtonClick("Java Conditionals If/Else Nesting")} */}
                     If/Else Nesting
             </button>
             <button className="lesson-btn"
-                onClick={() => handleButtonClick("Java Conditionals Ternary")}>
+                onClick={() => null}>
+                {/* onClick={() => handleButtonClick("Java Conditionals Ternary")} */}
                     Ternary
             </button>
             <button className="lesson-btn"
-                onClick={() => handleButtonClick("Java Switch Syntax")}>
+                onClick={() => null}>
+                {/* onClick={() => handleButtonClick("Java Switch Syntax")} */}
                     Switch Syntax
             </button>
             <button className="lesson-btn"
-                onClick={() => handleButtonClick("Java Switch Expressions")}>
+                onClick={() => null}>
+                {/* onClick={() => handleButtonClick("Java Switch Expressions")} */}
                     Switch Expressions
             </button>
             <button className="lesson-btn"
-                onClick={() => handleButtonClick("Java Switch Cases")}>
+                onClick={() => null}>
+                {/* onClick={() => handleButtonClick("Java Switch Cases")} */}
                     Switch Cases
             </button>
             <button className="lesson-btn"
-                onClick={() => handleButtonClick("Java Methods Break Statement")}>
+                onClick={() => null}>
+                {/* onClick={() => handleButtonClick("Java Methods Break Statement")} */}
                     Break Statement
             </button>
             <button className="lesson-btn"
-                onClick={() => handleButtonClick("Java Methods Continue Statement")}>
+                onClick={() => null}>
+                {/* onClick={() => handleButtonClick("Java Methods Continue Statement")} */}
                     Continue Statement
             </button>
         </div>
