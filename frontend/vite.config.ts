77e9fca90de7b57015ccc6bb18c273987719b815
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-08-14 17:04:45 -0400

    Change config back

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_REACT_APP_URL,
-        changeOrigin: true,
-      },
-    },
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 3000,
   },
-});
+})
