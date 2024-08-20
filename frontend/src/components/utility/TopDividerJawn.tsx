b4f909f5282e6fe9d837557758ccb28167671a49
Nina McNair <nina@Ninas-MacBook-Pro.local>
2024-08-19 20:39:52 -0400

    Update styles

---
+++ frontend/src/components/utility/TopDividerJawn.tsx
@@ -0,0 +1,9 @@
+const TopDividerJawn = () => {
+
+    return (
+        <div className="top-divider-container">
+            <div className="top-divider"></div>
+        </div>
+    )
+}
+export default TopDividerJawn;
