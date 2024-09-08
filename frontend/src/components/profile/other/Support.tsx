d5e3c78fc9aa53a12cc91925eb09eb26a6b8ba99
Nina DeJesus <nina.emiko.mcnair@gmail.com>
2024-09-08 12:24:18 -0400

    Updated profile/language containers

---
+++ frontend/src/components/profile/other/Support.tsx
@@ -0,0 +1,36 @@
+import { TERMS_AND_CONDITIONS } from "../../../helpers/termsAndConditionsConstants";
+
+const Support = ({ props }: { props: any; }) => {
+
+    return (
+        <div className="update-jawn">
+            <div className="terms-section">
+                <div className="terms-title">
+                    Support
+                </div>
+            </div>
+            <div className="terms-section">
+                <div className="terms-sub-title">
+                    FAQ
+                </div>
+                <div className="terms-text">
+                    
+                </div>
+            </div>
+            <div className="terms-section">
+                <div className="terms-sub-title">
+                    Contact Us
+                </div>
+                <div className="terms-text">
+
+                </div>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+        </div>
+    )
+}
+export default Support;
---
+++ frontend/src/helpers/languageDescriptionsHelper.tsx
@@ -0,0 +1,4 @@
+export const LANGUAGE_DESCRIPTIONS = {
+    JAVA: `Java is a high-level, object-oriented programming language. It's widely used for building robust, secure, and scalable applications, from web and mobile apps to enterprise and embedded systems.`,
+    JAVASCRIPT: `JavaScript is a versatile, high-level programming language primarily used for adding interactive and dynamic features to websites.`
+}
